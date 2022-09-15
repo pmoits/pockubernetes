204b4cce4b26e2e0aee90d319e38e3b63febccab
wagner <wagner.duarte@itsgroup.com.br>
2022-09-14 22:48:36 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const csvtojson = require('csvtojson');
+const bodyParser = require('body-parser');
+const jsontocsv = require('json2csv');
+const port = 8080;
+
+app.get("/data", (req, res) => {
+  const file = __dirname + '/data.csv';
+  csvtojson().fromFile(file)
+    .then(skills => {
+      res.send(skills);
+    }).catch(err => {
+      if (error) {
+        res.send(JSON.parse(error));
+      }
+    });
+});
+
+app.use(bodyParser.json());
+app.post("/data", (req, res) => {
+  const file = __dirname + '/data.csv';
+  const csvreq = jsontocsv.parse(req.body);
+  const csvArray = csvreq.split('\n');
+  const csvline = '\n' + csvArray[1];
+  fs.appendFile(file, csvline, (err) => {
+    if (err) console.error('Couldn\'t append the data');
+    res.send('{ "message":"Linha incluida ao CSV" }')
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`);
+});
